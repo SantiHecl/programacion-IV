14c265296ba18b7dc21bc69c196c12594dbdf4e7
JulianBentancor <juliaanbentancor@gmail.com>
2024-10-21 15:40:28 -0300

    Agregar archivos de proyecto.

---
+++ CuponProyecto/Models/CategoriaModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CuponProyecto.Models
+{
+    public class CategoriaModel
+    {
+        [Key]
+        public int Id_Categoria { get; set; }
+        public string Nombre { get; set; }
+        public virtual ICollection<Cupon_CategoriaModel>? Cupones_Categorias { get; set; }
+    }
+}
---
+++ CuponProyecto/Models/CuponModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CuponProyecto.Models
+{
+    public class CuponModel
+    {
+        [Key]
+        public int Id_Cupon { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal PorcentajeDto { get; set; }
+        public decimal ImportePromo { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+
+        public int Id_Tipo_Cupon { get; set; }
+        public bool Activo { get; set; }
+        public virtual ICollection<Cupon_CategoriaModel>? Cupones_Categorias { get; set; }
+        [ForeignKey("Id_Tipo_Cupon")]
+        public virtual Tipo_CuponModel? Tipo_Cupon { get; set; }
+    }
+}
